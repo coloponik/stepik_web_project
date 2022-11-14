64f87c72b6e8ca93b2917e7c7bc66a1822ee6b5f
Alisa Ponikarovskaya <coloponik@gmail.com>
2022-11-14 15:17:13 +0300

    MySQL database setup and models creation fo qa_app

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'stepik_web',
+        'USER': 'stepik',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-raiting')
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='user_like_que')
+    objects = QuestionManager()
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
