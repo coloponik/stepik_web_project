c4a0e6e580ffc51924c7df3ce8c8b865429dfe41
Alisa Ponikarovskaya <coloponik@gmail.com>
2022-11-15 12:03:35 +0300

    Update init.sh

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -2,8 +2,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# pythonpath = '/home/user1/web/ask'
-# bind = '0.0.0.0:8000'
+# pythonpath = '/home/user1/web/ask/ask'
+# bind = '0.0.0.0:8080'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ask.settings')
 
--- init.sh
+++ init.sh
@@ -1,2 +1,7 @@
-sudo ln -s /home/user1/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+cd /home/user1/web/ask/ask
+gunicorn --pythonpath="/home/user1/web/ask" -b 0.0.0.0:8080 wsgi:application --reload
+cd /etc/nginx/sites-enabled
+sudo fuser -k 80/tcp
+sudo unlink default
 sudo nginx -c /etc/nginx/sites-enabled/test.conf
+sudo service nginx restart
